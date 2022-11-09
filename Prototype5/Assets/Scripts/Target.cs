2ea4deaf67b6bdc1839361dfc03fe9f838ae7dcd
Moeketsi Moloi <moloimason@gmail.com>
2022-11-09 11:02:47 -0800

    update

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public int pointValue = 1;
-    public float minSpeed = 10;
-    public float maxSpeed = 20;
-    public float MaxTorque = 10;
+    public float minSpeed = 6;
+    public float maxSpeed = 14;
+    public float MaxTorque = 40;
     private Rigidbody2D _targetRb;
     // Start is called before the first frame update
     void Start()
     {
         _targetRb = GetComponent<Rigidbody2D>();
-        _targetRb.AddForce(Vector2.up * minSpeed, ForceMode2D.Impulse);
+        _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
+        _targetRb.AddTorque(RandomizeTorque());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private float RandomizeForce()
+    {
+        return Random.Range(minSpeed, maxSpeed);
+    }
+    private float RandomizeTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
     }
 }
